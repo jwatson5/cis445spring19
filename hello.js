e894a0c308f21e10836ddc8c257f60ab9e539771
Watson <jswatson@gmail.com>
2019-01-28 16:38:06 +0000

    daily 1/28/19

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log("hello, we are here");
+const http = require('http');
+
+http.createServer(function(request, response) {
+    //console.log(request);
+    response.writeHead(200);
+    response.write("<html><body><p>Hello, dog is running.</p>");
+    setTimeout(function() {
+        response.write("<p>dog is really tired</p></body></html>");
+        response.end();
+    }, 5000);
+}).listen(8080);
+
+console.log('listening on port 8080...');
